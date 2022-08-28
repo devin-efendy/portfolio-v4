2fdd091f2fa34de62db43a8dc0cdf92f8255c582
devin-efendy <devinefendy.dev@gmail.com>
2022-08-28 13:49:20 -0500

    add content for experience

--- data/experience.ts
+++ data/experience.ts
@@ -5,7 +5,7 @@ const experience = [
     companyName: 'Priceline',
     date: 'May 2022 – Aug 2022',
     description:
-      "I was primarily working on integrating Okta authentication and authorization for my team's new application to protect pages and APIs. This also allow users to have seamless transition between Priceline's applications. I also improved autocomplete functionality for one of Priceline's partner which provides better usability and support custom dictionaries. Lastly, I organized and hosted company-wide technology showcase event with 150+ attendees",
+      "I was primarily working on integrating Okta authentication and authorization for my team's new application to protect pages and APIs. This also allow users to have seamless transition between Priceline's applications through SSO. I also improved autocomplete functionality for one of Priceline's partner which provides better usability and support custom dictionaries. Lastly, I organized and hosted company-wide technology showcase event with 150+ attendees",
     location: 'Winnipeg, MB (remote)',
     technologies:
       'TypeScript, React, Node.js, Remix, GraphQL, Jest, React Testing Library',
@@ -16,10 +16,10 @@ const experience = [
     companyName: 'Priceline',
     date: 'Sep 2021 – Dec 2021',
     description:
-      "Worked on Next.js application to unify landing pages of Priceline's products. This involves building React components, writing tests, and integrating feature toggles and CMS. In addition to that, I built a GitHub Action that sends alerts to a Slack channel about the progress of Contentful migrations.",
+      "Worked on Next.js application to unify landing pages of Priceline's products. This involves building React components, writing tests, and using feature toggles and CMS. In addition to that, I built a GitHub Action that sends alerts to a Slack channel about the progress of Contentful migrations.",
     location: 'Winnipeg, MB (remote)',
     technologies:
-      'TypeScript, React, Node.js, Next.js, HTML, CSS, Jest, React Testing Library, Contentful, GitHub Action',
+      'TypeScript, React, Node.js, Next.js, HTML, CSS, Jest, React Testing Library, Contentful, LaunchDarkly, GitHub Action',
   },
   {
     id: 'ubisoft-00',
@@ -27,7 +27,7 @@ const experience = [
     companyName: 'Ubisoft Winnipeg',
     date: 'Jan 2021 – Apr 2021',
     description:
-      'At Ubisoft, I worked with C#, .NET, .NET Core, Postgre SQL, and WPF applications. I was primarily developing new features on WPF application and .NET backend application. It was very exciting to get some exposure to game development! I had opportunities to work with Unreal Engine and Unity as well.',
+      'At Ubisoft, I worked with C#, .NET, .NET Core, Postgre SQL, and WPF applications. I was primarily developing new features on internal tool that consists of Windows desktop application and .NET backend application. It was very exciting to get some exposure to game development! I had opportunities to work with Unreal Engine and Unity as well.',
     location: 'Winnipeg, MB (remote)',
     technologies: 'C#, .NET, .NET Core, PostgreSQL, WPF, OOP, REST API',
   },
@@ -37,7 +37,7 @@ const experience = [
     companyName: 'Bold Commerce',
     date: 'May 2020 – Aug 2020',
     description:
-      "During my co-op term at Bold, I was working in Checkout Plugin & API team. The team develops plugin features and API endpoints for Bold Checkout. One of my main resposibility is to revamp one of Bold's internal plugin which gives customer a discount when they share their purchases to Facebook or Twitter. Doing so requires refactoring the code while adhering to Software Engineering principles, reconfiguring deployment pipeline, and researching compatible packages as well as upgrading legacy dependencies.",
+      "During my co-op term at Bold, I was working in Checkout Plugin & API team. The team develops plugin features and API endpoints for Bold Checkout. One of my main resposibility is to revamp one of Bold's internal plugin which gives customer a discount when they share their purchases to Facebook or Twitter. Doing so requires refactoring the code while adhering to Software Engineering principles (such as SOLID and Dependency Injection), reconfiguring deployment pipeline, and researching compatible packages as well as upgrading legacy dependencies.",
     location: 'Winnipeg, MB (remote)',
     technologies:
       'PHP, Laravel, JavaScript, React, Node.js, MySQL, Docker, OOP',
